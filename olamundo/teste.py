09bc2cc0931358c89d7e90724b88807310924437
willian rafael <willianrafael050403@gmail.com>
2024-10-25 09:09:57 -0300

    img no python

---
+++ olamundo/teste.py
@@ -0,0 +1,10 @@
+numero = float (input("digite a sua altura: ") )
+print (numero)
+
+numero2 = float(input("Digite esse peso:") )
+print( numero2 )
+
+img = (numero2 / (numero**2))
+print( f"peso {numero2} / altura² {numero} = {img}")
+
+print(" meu indice de massa corporal é igual {img:.2f}")
